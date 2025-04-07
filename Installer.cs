873cdb1bd80ffcb7231b1b6367d33e87ca14b259
Ender <67215373+EndermanSUPREME@users.noreply.github.com>
2025-04-07 16:55:14 -0400

    Created basic installer form with progress bar

---
+++ Installer.cs
@@ -0,0 +1,60 @@
+using SysView;
+
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+public class InstallerPrompt : Form
+{
+    ProgressBar pb = new ProgressBar();
+    SystemData sd = new SystemData();
+    bool lightMode;
+
+    public InstallerPrompt()
+    {
+        lightMode = sd.UsingLightTheme();
+
+        this.Text = "Windows Driver Reinstaller";
+        this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        this.Size = new Size(460, 300);
+        this.StartPosition = FormStartPosition.CenterScreen;
+
+        if (!lightMode)
+        {
+            this.BackColor = Color.FromArgb(32, 32, 32);
+            this.ForeColor = Color.White;
+        }
+
+        pb.Location = new Point(20, 20);
+        pb.Size = new Size(340, 25);
+        pb.Minimum = 0;
+        pb.Maximum = 100;
+        pb.Style = ProgressBarStyle.Continuous;
+        this.Controls.Add(pb);
+
+        Thread t = new Thread(new ThreadStart(ShowProgress));
+        t.Start();
+    }
+
+//==============================================================================
+//==============================================================================
+//==============================================================================
+
+    void ShowProgress()
+    {
+        while (pb.Value < 100)
+        {
+            pb.Value += 1;
+            Thread.Sleep(1000);
+        }
+    }
+
+    // This method provides external objects to trigger this winform
+    public void ShowPrompt()
+    {
+        Application.EnableVisualStyles();
+        Application.Run(new InstallerPrompt());
+    }
+}
